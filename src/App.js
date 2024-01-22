66e7e919a9ee6a8e1c52562f004cf345601f998c
Kuldeepkushwah06 <144555844+Kuldeepkushwah06@users.noreply.github.com>
2024-01-22 23:46:38 +0530

    empty cart completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+import FooterComponet from './components/FooterComponet';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <div>
+        <Navbar/>
+      </div>
+      <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+      </Routes>
+      </div>
+      <div>
+        <FooterComponet/>
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/EmptyCart.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const EmptyCart = () => {
+    return (
+        
+            <div className='outline'>
+                
+                <div className=' w-full flex flex-col justify-center items-center gap-4 outline '>
+                    <img
+                        src='https://www.smartdrugsx.com/assets/img/empty-cart1.png'
+                        className='w-48'
+                    ></img>
+                    <div className='flex flex-col justify-center items-center'>
+                        <h1 className=' text-xl font-semibold'>Empty Cart</h1>
+                        <p className='text-gray-400 text-md'>Looks like you haven't made </p>
+                        <p className='text-gray-400 text-md'>your choice yet... </p>
+                    </div>
+
+                    <NavLink to='/'>
+                        <button className='bg-green-700 text-black rounded-md p-1 font-semibold w-48 '>Shop Now</button>
+                    </NavLink>
+
+                    <div className='flex flex-col items-center justify-center '>
+                        <p className='text-green-500 text-sm font-light'>Check What we've got for you</p>
+                        <p className='text-gray-400 text-sm'>and get it swished!</p>
+                    </div>
+                </div>
+            </div>
+        
+    )
+}
+
+export default EmptyCart
